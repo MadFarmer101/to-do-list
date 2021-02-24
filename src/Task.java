f705079f72fad0edd159543d01c4368561b5ec83
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-02-24 11:27:51 +0100

    Generates getter and setter methods for all Task class fields + changes the name of boolean isDone to status

--- src/Task.java
+++ src/Task.java
@@ -4,5 +4,37 @@ public class Task {
     private String name;
     private  String project;
     private boolean isDone;
-    private Date dueDate;
+    private Date status;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public Date getDueDate() {
+        return status;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.status = dueDate;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
