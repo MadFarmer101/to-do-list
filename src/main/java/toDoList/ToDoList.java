679a57b416a7a8e8680216b86d3794ebf1c59af7
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-01 11:41:15 +0100

    Adds setting a due date and project name when adding a task object to the ToDo list

--- src/main/java/toDoList/ToDoList.java
+++ src/main/java/toDoList/ToDoList.java
@@ -1,5 +1,6 @@
 package toDoList;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,9 +11,16 @@ public class ToDoList {
         Scanner userInput = new Scanner(System.in);
         Task task = new Task();
 
-        System.out.println("Please enter Task's name: ");
+        System.out.println("Name: ");
         task.setName(userInput.nextLine());
-        
+
+        System.out.println("Project: ");
+        task.setProject(userInput.nextLine());
+
+        System.out.println("Due Date: ");
+        System.out.println("Date must be entered in a format: YYYY-MM-DD");
+        task.setDueDate(LocalDate.parse(userInput.nextLine()));
+
     }
 
 }
