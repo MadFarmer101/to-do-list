3823b03662dea8fc6f7062d036861c296a72c108
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-01 10:45:54 +0100

    Adds a method addTask in the ToDoList class, adds Scannerand Task instances and sets the Task's name that will equal user's input

--- src/main/java/toDoList/ToDoList.java
+++ src/main/java/toDoList/ToDoList.java
@@ -1,8 +1,18 @@
 package toDoList;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ToDoList {
     ArrayList<Task> toDoList = new ArrayList<>();
-    
+
+    public void addTask() {
+        Scanner userInput = new Scanner(System.in);
+        Task task = new Task();
+
+        System.out.println("Please enter Task's name: ");
+        task.setName(userInput.nextLine());
+        
+    }
+
 }
