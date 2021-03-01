501c39c43cdc76c075f4a085c4de15b26587f9dd
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-01 14:55:28 +0100

    Refactores ToDoListTest by creating and adding two tasks to the list in BeforeAll method and changes the title of the test to check for list size after adding tasks

--- src/test/java/toDoList/ToDoListTest.java
+++ src/test/java/toDoList/ToDoListTest.java
@@ -8,18 +8,25 @@ import java.io.InputStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ToDoListTest {
+    static  ToDoList toDoLy = new ToDoList();
 
-    @Test
-    void successfullyAddTaskToTheToDOList() {
-
-        ToDoList toDoLy = new ToDoList();
+    @BeforeAll
+    public static void createATask() {
         String input = "task1\nproject1\n2021-05-03";
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
 
         System.setIn(inputStream);
-
         toDoLy.addTask();
 
-        assertEquals(1, toDoLy.getToDoList().size());
+        String input2 = "task2\nproject2\n2021-04-15";
+        InputStream inputStream2 = new ByteArrayInputStream(input2.getBytes());
+
+        System.setIn(inputStream2);
+        toDoLy.addTask();
+    }
+
+    @Test
+    public void checksForCorrectListSize() {
+        assertEquals(2, toDoLy.getToDoList().size());
     }
 }
