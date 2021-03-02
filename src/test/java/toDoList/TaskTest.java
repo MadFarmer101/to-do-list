790d9b4046c7473597fcb4d3dcc3fd56888002a1
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-02 09:53:22 +0100

    Refactores tests in TaskTest that check for project and due date to check for equality on a task that is created with user input

--- src/test/java/toDoList/TaskTest.java
+++ src/test/java/toDoList/TaskTest.java
@@ -7,13 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.time.*;
 
 public class TaskTest {
     static Task task = new Task();
 
     @BeforeAll
-    public static void createTask() {
+    public static void setTaskFields() {
         String input = "task1\nproject1\n2021-05-03";
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
@@ -28,9 +27,12 @@ public class TaskTest {
 
     @Test
     void taskHasACorrectProject() {
-        Task task = new Task();
-        task.setProject("project1");
-        assertTrue(task.getProject().equals("project1"));
+        assertEquals("project1", task.getProject());
+    }
+
+    @Test
+    void taskHasACorrectDueDate() {
+        assertEquals("2021-05-03", task.getDueDate().toString());
     }
 
     @Test
@@ -42,13 +44,6 @@ public class TaskTest {
     void statusIsTrueWhenTaskIsMarkedAsDone() {
         task.markAsDone();
         assertTrue(task.status());
-    }
-
-    @Test
-    void taskHasACorrectDueDate() {
-        LocalDate deadline = LocalDate.parse("2021-03-03");
-        task.setDueDate(deadline);
-        assertTrue(task.getDueDate().toString().equals("2021-03-03"));
     }
 
 }
