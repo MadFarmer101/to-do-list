18da6640ce56fbe871cca5ace233e296d8443e43
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-10 13:57:31 +0100

    Adds editListMenu with all the options

--- src/main/java/toDoList/Menus.java
+++ src/main/java/toDoList/Menus.java
@@ -8,8 +8,18 @@ public class Menus {
         System.out.println("\n>> Pick an option:");
         System.out.println(">>> 1) Show ToDo List (by date or project)");
         System.out.println(">>> 2) Add New Task");
-        System.out.println(">>> 3) Edit Task (update, mark as done, remove)");
+        System.out.println(">>> 3) Edit List (update, mark as done, remove)");
         System.out.println(">>> 4) Save and Quit");
+        System.out.println(">> Please enter your choice (1-4):");
+    }
+
+    public static void editListMenu() {
+        System.out.println("\n>> Edit List");
+        System.out.println("\n>> Pick an option:");
+        System.out.println(">>> 1) Edit Task (change name, project or dueDate)");
+        System.out.println(">>> 2) Mark Task as done");
+        System.out.println(">>> 3) Remove a Task");
+        System.out.println(">>> 4) Return to Main Menu");
         System.out.println(">> Please enter your choice (1-4):");
     }
 }
