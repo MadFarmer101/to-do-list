0872cf14f1e3bf75938c25edc71c66d7399b62d3
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-10 13:51:15 +0100

    Adds a mainMenu method that prints the welcome message and asks for a choice

--- src/main/java/toDoList/Menus.java
+++ src/main/java/toDoList/Menus.java
@@ -2,7 +2,14 @@ package toDoList;
 
 public class Menus {
 
-    public static void mainMenu() {
-        
+    public static void mainMenu(int tasksCompleted, int notCompletedTasks) {
+        System.out.println("\n>> Welcome to ToDoListy");
+        System.out.println(">> You have " + notCompletedTasks + " tasks todo and " + tasksCompleted + " tasks are done!");
+        System.out.println("\n>> Pick an option:");
+        System.out.println(">>> 1) Show ToDo List (by date or project)");
+        System.out.println(">>> 2) Add New Task");
+        System.out.println(">>> 3) Edit Task (update, mark as done, remove)");
+        System.out.println(">>> 4) Save and Quit");
+        System.out.println(">> Please enter your choice (1-4):");
     }
 }
