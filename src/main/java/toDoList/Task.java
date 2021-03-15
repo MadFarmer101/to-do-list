33e12cfbea51c1fb38a9410d3cf8515fafd8c232
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-15 12:30:50 +0100

    Adds description for Task class and throws a NullPointerException if task's name is empty

--- src/main/java/toDoList/Task.java
+++ src/main/java/toDoList/Task.java
@@ -4,21 +4,46 @@ import java.io.Serializable;
 import java.time.*;
 import java.util.Scanner;
 
+/**
+ * This is a model class and it represents a Task object
+ * It contains necessary fields and methods to operate on task object.
+ *
+ * @author  Janko
+ * @version 1.0
+ * @since   2021-03-15
+ **/
 public class Task implements Serializable {
+
     private String name;
     private String project;
     private boolean status;
     private LocalDate dueDate;
 
+    /**
+     * Creating an object of Task class
+     */
     public Task() {
         this.status = false;
     }
 
     /**
-     * @return task's name.
+     * A method to get the task title
+     * @return a String containing the title of a task
      */
     public String getName() {
         return name;
+    }
+
+    /**
+     * A method to set the title of a Task object
+     * @param name A String that holds the title of a task and it cannot be empty or null.
+     * @throws NullPointerException if title is null or empty string
+     */
+    public void setName(String name) throws NullPointerException {
+        if (name.trim().equals("")) {
+            throw new NullPointerException("REQUIRED: Name can't be empty.");
+        }
+        this.name = name;
     }
 
     /**
@@ -54,13 +79,6 @@ public class Task implements Serializable {
      */
     public void setProject(String project) {
         this.project = project;
-    }
-
-    /**
-     * Setts a task's name.
-     */
-    public void setName(String name) {
-        this.name = name;
     }
 
     /**
